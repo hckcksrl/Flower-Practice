0f4041ed20ac695b9604a1a8418842040623ceb8
hckcksrl@gmail.com <hckcksrl@hckcksrlui-MacBook-Pro.local>
2019-03-13 18:34:41 +0900

    data

---
+++ Route/config.js
@@ -0,0 +1,144 @@
+const flowers = [
+  {
+    id: 1,
+    url: require("../assets/1.jpg"),
+    smallname: "플라워",
+    bigname: "장미",
+    like: 230
+  },
+  {
+    id: 2,
+    url: require("../assets/2.jpg"),
+    smallname: "플라워",
+    bigname: "장미",
+    like: 200
+  },
+  {
+    id: 3,
+    url: require("../assets/3.jpg"),
+    smallname: "플라워",
+    bigname: "장미",
+    like: 150
+  },
+  {
+    id: 4,
+    url: require("../assets/4.jpg"),
+    smallname: "플라워",
+    bigname: "장미",
+    like: 350
+  },
+  {
+    id: 5,
+    url: require("../assets/1.jpg"),
+    smallname: "플라워",
+    bigname: "장미",
+    like: 230
+  },
+  {
+    id: 6,
+    url: require("../assets/2.jpg"),
+    smallname: "플라워",
+    bigname: "장미",
+    like: 200
+  },
+  {
+    id: 7,
+    url: require("../assets/3.jpg"),
+    smallname: "플라워",
+    bigname: "장미",
+    like: 150
+  },
+  {
+    id: 8,
+    url: require("../assets/4.jpg"),
+    smallname: "플라워",
+    bigname: "장미",
+    like: 350
+  },
+  {
+    id: 9,
+    url: require("../assets/1.jpg"),
+    smallname: "플라워",
+    bigname: "장미",
+    like: 230
+  },
+  {
+    id: 10,
+    url: require("../assets/2.jpg"),
+    smallname: "플라워",
+    bigname: "장미",
+    like: 200
+  },
+  {
+    id: 11,
+    url: require("../assets/3.jpg"),
+    smallname: "플라워",
+    bigname: "장미",
+    like: 150
+  },
+  {
+    id: 12,
+    url: require("../assets/4.jpg"),
+    smallname: "플라워",
+    bigname: "장미",
+    like: 350
+  },
+  {
+    id: 13,
+    url: require("../assets/1.jpg"),
+    smallname: "플라워",
+    bigname: "장미",
+    like: 230
+  },
+  {
+    id: 14,
+    url: require("../assets/2.jpg"),
+    smallname: "플라워",
+    bigname: "장미",
+    like: 200
+  },
+  {
+    id: 15,
+    url: require("../assets/3.jpg"),
+    smallname: "플라워",
+    bigname: "장미",
+    like: 150
+  },
+  {
+    id: 16,
+    url: require("../assets/4.jpg"),
+    smallname: "플라워",
+    bigname: "장미",
+    like: 350
+  },
+  {
+    id: 17,
+    url: require("../assets/1.jpg"),
+    smallname: "플라워",
+    bigname: "장미",
+    like: 230
+  },
+  {
+    id: 18,
+    url: require("../assets/2.jpg"),
+    smallname: "플라워",
+    bigname: "장미",
+    like: 200
+  },
+  {
+    id: 19,
+    url: require("../assets/3.jpg"),
+    smallname: "플라워",
+    bigname: "장미",
+    like: 150
+  },
+  {
+    id: 20,
+    url: require("../assets/4.jpg"),
+    smallname: "플라워",
+    bigname: "장미",
+    like: 350
+  }
+];
+
+export default flowers;
